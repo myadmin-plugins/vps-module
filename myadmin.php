ce2e922a477568be60df176490f6c5f646745fb3
Joe Huss <detain@interserver.net>
2017-06-02 04:40:33 -0400

    fixing loader

--- myadmin.php
+++ myadmin.php
@@ -15,7 +15,7 @@ return [
 	'version' => '1.0.0',
 	'type' => 'addon',
 	'hooks' => [
-		'vps.load_addons' => ['Detain\MyAdminVpsVps', 'Load'],
+		'vps.load_addons' => ['Detain\MyAdminVpsVps\Plugin', 'Load'],
 		/* 'function.requirements' => ['Detain\MyAdminVpsVps\Plugin', 'Requirements'],
 		'licenses.settings' => ['Detain\MyAdminVpsVps\Plugin', 'Settings'],
 		'licenses.activate' => ['Detain\MyAdminVpsVps\Plugin', 'Activate'],
