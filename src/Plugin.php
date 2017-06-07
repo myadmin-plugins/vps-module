1a56affb33b3761fa9f8afaed9cc3503dafef712
Joe Huss <detain@interserver.net>
2017-06-07 11:31:22 -0400

    updates

--- src/Plugin.php
+++ src/Plugin.php
@@ -47,6 +47,6 @@ class Plugin {
 
 	public static function Settings(GenericEvent $event) {
 		$settings = $event->getSubject();
-		$settings->add_dropdown_setting('vps', 'General', 'outofstock_vps', 'Out Of Stock VPS', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_VPS'), array('0', '1'), array('No', 'Yes', ));
+		$settings->add_dropdown_setting('vps', 'General', 'outofstock_vps', 'Out Of Stock VPS', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_VPS'), array('0', '1'), array('No', 'Yes'));
 	}
 }
