a0103c0417a198189df3443fb5715095ed9b1ded
Joe Huss <detain@interserver.net>
2017-06-07 11:09:14 -0400

    updates to the module

--- myadmin.php
+++ myadmin.php
@@ -5,21 +5,21 @@
  - add way to call/hook into install/uninstall
 */
 return [
-	'name' => 'Vps Licensing VPS Addon',
-	'description' => 'Allows selling of Vps Server and VPS License Types.  More info at https://www.netenberg.com/vps.php',
-	'help' => 'It provides more than one million end users the ability to quickly install dozens of the leading open source content management systems into their web space.  	Must have a pre-existing cPanel license with cPanelDirect to purchase a vps license. Allow 10 minutes for activation.',
+	'name' => 'MyAdmin VPS Module for MyAdmin',
+	'description' => 'Allows selling of Vps Module',
+	'help' => '',
 	'module' => 'vps',
 	'author' => 'detain@interserver.net',
-	'home' => 'https://github.com/detain/myadmin-vps-vps-addon',
-	'repo' => 'https://github.com/detain/myadmin-vps-vps-addon',
+	'home' => 'https://github.com/detain/myadmin-vps-module',
+	'repo' => 'https://github.com/detain/myadmin-vps-module',
 	'version' => '1.0.0',
-	'type' => 'addon',
+	'type' => 'module',
 	'hooks' => [
 		'vps.load_processing' => ['Detain\MyAdminVps\Plugin', 'Load'],
+		'vps.settings' => ['Detain\MyAdminVps\Plugin', 'Settings'],
 		/* 'function.requirements' => ['Detain\MyAdminVps\Plugin', 'Requirements'],
-		'licenses.settings' => ['Detain\MyAdminVps\Plugin', 'Settings'],
-		'licenses.activate' => ['Detain\MyAdminVps\Plugin', 'Activate'],
-		'licenses.change_ip' => ['Detain\MyAdminVps\Plugin', 'ChangeIp'],
+		'vps.activate' => ['Detain\MyAdminVps\Plugin', 'Activate'],
+		'vps.change_ip' => ['Detain\MyAdminVps\Plugin', 'ChangeIp'],
 		'ui.menu' => ['Detain\MyAdminVps\Plugin', 'Menu'] */
 	],
 ];
