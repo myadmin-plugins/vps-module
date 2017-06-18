a0e2c9e0edda332287eba2c32862e33ba05c3c12
Joe Huss <detain@interserver.net>
2017-06-18 01:48:08 -0400

    fixs to camelCase for psr

--- src/Plugin.php
+++ src/Plugin.php
@@ -19,7 +19,7 @@ class Plugin {
 	public static function getHooks() {
 		return [
 			'vps.load_processing' => [__CLASS__, 'Load'],
-			'vps.settings' => [__CLASS__, 'Settings'],
+			'vps.settings' => [__CLASS__, 'getSettings'],
 		];
 	}
 
@@ -60,7 +60,7 @@ class Plugin {
 			})->register();
 	}
 
-	public static function Settings(GenericEvent $event) {
+	public static function getSettings(GenericEvent $event) {
 		$module = 'vps';
 		$settings = $event->getSubject();
 		$settings->add_text_setting($module, 'Credentials', 'webuzo_license_key', 'Webuzo License Key:', 'API Credentials for Webuozo', $settings->get_setting('WEBUZO_LICENSE_KEY'));
