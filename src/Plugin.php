6f35252358dab7d555dca6b7b518b7aadf6543bb
Joe Huss <detain@interserver.net>
2018-12-11 09:18:09 -0500

    updates setting setting target

--- src/Plugin.php
+++ src/Plugin.php
@@ -238,11 +238,13 @@ class Plugin
 		$settings->add_dropdown_setting(self::$module, _('Slice Amounts'), 'vps_bw_type', _('Bandwidth Limited by Total Traffic or Throttling'), _('Enable/Disable Sales Of This Type'), $settings->get_setting('VPS_BW_TYPE'), ['1', '2'], ['Throttled in mbps', 'Total GBytes Used']);
 		$settings->add_text_setting(self::$module, _('Slice Amounts'), 'vps_slice_bw', _('Bandwidth Limit Per Slice in Mbits/s  or Gbytes'), _('Amount of Bandwidth per slice.'), $settings->get_setting('VPS_SLICE_BW'));
 		$settings->add_text_setting(self::$module, _('Slice Amounts'), 'vps_slice_max', _('Max Slices Per Order'), _('Maximum amount of slices any one VPS can be.'), $settings->get_setting('VPS_SLICE_MAX'));
+        $settings->setTarget('module');
 		$settings->add_master_checkbox_setting(self::$module, 'Server Settings', self::$module, 'available', 'vps_available', 'Auto-Setup', '<p>Choose which servers are used for auto-server Setups.</p>');
 		$settings->add_master_label(self::$module, 'Server Settings', self::$module, 'active_services', 'Active VPS', '<p>The current number of active VPS.</p>', 'count(vps.vps_id) as active_services');
 		$settings->add_master_text_setting(self::$module, 'Server Settings', self::$module, 'server_max', 'vps_server_max', 'Max VPS', '<p>The Maximum number of VPS that can be running on each server.</p>');
 		$settings->add_master_label(self::$module, 'Server Settings', self::$module, 'active_slices', 'Active Slices', '<p>The current total slices from active VPS.</p>', 'sum(vps.vps_slices) as active_slices');
 		$settings->add_master_text_setting(self::$module, 'Server Settings', self::$module, 'server_max_slices', 'vps_server_max_slices', 'Max Slices', '<p>The Maximum number of total slices that can be running on each server.</p>');
 		$settings->add_dropdown_setting(self::$module, _('Out of Stock'), 'outofstock_vps', _('Out Of Stock VPS'), _('Enable/Disable Sales Of This Type'), $settings->get_setting('OUTOFSTOCK_VPS'), ['0', '1'], ['No', 'Yes']);
+        $settings->setTarget('global');
 	}
 }
